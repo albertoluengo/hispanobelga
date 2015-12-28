c475e86fd195e864ec884ab90bf6b30e2d1db4b7
Alberto Luengo Cabanillas <cabiwan@gmail.com>
2015-12-29 00:24:04 +0800

    [REF] Se cambian los textos.

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,9 +1,14 @@
 //console.log('This would be the main JS file.');
 
-function toggle_visibility(id) {
-    var e = document.getElementById(id);
-    if(e.style.display == 'block')
-       e.style.display = 'none';
+function toggle_visibility(id_right, code, id_wrong) {
+    if (code == '11-10-15') {
+        var e = document.getElementById(id_right);
+    }
+    else {
+        var e = document.getElementById(id_wrong);
+    }
+    if (e.style.display == 'block')
+        e.style.display = 'none';
     else
-       e.style.display = 'block';
+        e.style.display = 'block';
 }
